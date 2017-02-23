1d036ea33fbf6d49b1fea199ae378716ad19e552
Rahul Jadav <rj.jadav22@gmail.com>
2017-02-23 20:29:53 +0530

    log and procfile

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000 , function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
