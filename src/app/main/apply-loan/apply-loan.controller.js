78f8c398b5a33dfbeefa7a65d365074e6c251189
Rahul Jadav <rj.jadav22@gmail.com>
2017-03-20 10:48:54 +0530

    design changes

--- src/app/main/apply-loan/apply-loan.controller.js
+++ src/app/main/apply-loan/apply-loan.controller.js
@@ -14,9 +14,20 @@
     apply.next = next;
     apply.prev = prev;
     apply.uploadSalarySlips = uploadSalarySlips;
+    apply.getAddress = getAddress;
+    apply.getPermanentAddress = getPermanentAddress;
 
     apply.active = 0;
     apply.uploadedSalarySlips = [];
+    apply.showCityState = false;
+    apply.showPermanentCityState = false;
+    apply.loanDetails = {
+      loanAmount: 100000,
+      tenure: 12,
+      permanentAddress:{
+        sameAsCurrentAddress: true
+      },
+    };
 
     function next(){
       apply.active += 1;
@@ -27,9 +38,20 @@
     }
 
     function uploadSalarySlips(salarySlips){
-
       for(var i=0; i< salarySlips.length; i++){
         apply.uploadedSalarySlips.push(salarySlips[i]);
+      }
+    }
+
+    function getAddress(pincode){
+      if(pincode.length == 6){
+        apply.showCityState = true;
+      }
+    }
+
+    function getPermanentAddress(pincode){
+      if(pincode.length == 6){
+        apply.showPermanentCityState = true;
       }
     }
   }
